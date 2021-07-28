49e1d18b6372c8e9237da305dcbde219ba804ac4
ujeon_jang <wj.jang@newploy.net>
2021-07-28 12:11:12 +0900

    이미지 선택 컨트롤러를 활용한 델리게이트 패턴 구현

--- Delegate-ImagePicker/ViewController.swift
+++ Delegate-ImagePicker/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imgView: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func pick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        
+        picker.delegate = self
+        
+        self.present(picker, animated: false)
     }
-
-
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true) { () in
+            let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다.🧐", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true) { () in
+            let img = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            self.imgView.image = img
+        }
+    }
 }
 
